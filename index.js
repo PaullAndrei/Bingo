9c84f32e4ec5ea64be490988d1f6db7b5c5c7377
Paul Andrei G. Gulapa <pggulapa.wup@gmail.com>
2024-03-15 09:28:42 +0800

    done

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function newNumber(maxNumber) {
         console.log(`New number ${x} is invalid.`);
     }
 
-    if (B == true && I == true) {
+    if (B == true && I == true && N == true && G == true && O == true) {
         console.log(`BINGOOOOO!!!`);
     }
 }
