68020aad97e5771f004a47d91f7775d1de757fcf
Paul Andrei G. Gulapa <pggulapa.wup@gmail.com>
2024-03-15 09:25:11 +0800

    js

--- index.js
+++ index.js
@@ -19,16 +19,16 @@ function newNumber(maxNumber) {
     if(x <= 15) {
         console.log(`New number ${x} belongs to "B"`);
         B = true;
-    } else if (x >= 1 && x <= 75) {
+    } else if (x >= 16 && x <= 30) {
         console.log(`New number ${x} belongs to "I"`);
         I = true;
-    }else if (x >= 1 && x <= 75) {
+    }else if (x >= 30 && x <= 45) {
         console.log(`New number ${x} belongs to "N"`);
         N = true;
-    }else if (x >= 1 && x <= 75) {
+    }else if (x >= 45 && x <= 60) {
         console.log(`New number ${x} belongs to "G"`);
         G = true;
-    }else if (x >= 1 && x <= 75) {
+    }else if (x >= 60 && x <= 75) {
         console.log(`New number ${x} belongs to "O"`);
         O = true;
     }else {
